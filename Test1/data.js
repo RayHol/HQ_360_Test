5eafa86d194f0b0f1ca42463010e9b11133036cd
Ray Holder <46814365+RayHol@users.noreply.github.com>
2025-03-23 16:22:15 +0000

    Update data.js

--- Test1/data.js
+++ Test1/data.js
@@ -34,7 +34,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "yaw": 0.0446891358250916,
         "pitch": 0.10202175224063836,
-        "fov": 2.5708
+        "fov": 1
       },
       "linkHotspots": [],
       "infoHotspots": []
